c4b79cfd2ab4111ff9b8360a3f95ca23e1b08c58
kasim <chu.chongmeng@gmail.com>
2022-02-06 19:26:25 +0800

    Update forecast.sh
    Added correct string and modify a bit the format.

--- scripts/forecast.sh
+++ scripts/forecast.sh
@@ -5,8 +5,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_forecast() {
-  local format=$(get_tmux_option @forecast-format "%C+%t+%w")
-  local location=$(get_tmux_option @forecast-location "Central,%%20Hong%%20Kong")
+  local format=$(get_tmux_option @forecast-format "%l:%c+%t+%w")
+  local location=$(get_tmux_option @forecast-location "Central,%20Hong%20Kong")
   local char_limit=$(get_tmux_option @forecast-char-limit 75)
   local forecast=$(curl http://wttr.in/$location?format=$format)
   echo ${forecast:0:$char_limit}
